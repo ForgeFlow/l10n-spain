11046cae895b71e26f6126e1e28ea205b39a0dc0
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-03-28 11:16:24 +0100

    fixup

--- l10n_es_aeat/models/res_company.py
+++ l10n_es_aeat/models/res_company.py
@@ -4,6 +4,7 @@
 import logging
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 from odoo.tools import ormcache
 
 _logger = logging.getLogger(__name__)
@@ -70,29 +71,33 @@ class ResCompany(models.Model):
             or False
         )
 
-    # TODO: search first by AEAT scope
     def get_certificates(self):
         company = self or self.env.user.company_id
         today = fields.Date.today()
+
         domain = [
             ("company_id", "=", company.id),
-            ("certificate_id", "!=", False),
-            "|",
-            ("certificate_id.date_start", "=", False),
-            ("certificate_id.date_start", "<=", today),
-            "|",
-            ("certificate_id.date_end", "=", False),
-            ("certificate_id.date_end", ">=", today),
+            "|",  # Or condition for date_start
+            ("date_start", "=", False),
+            ("date_start", "<=", today),
+            "|",  # Or condition for date_end
+            ("date_end", "=", False),
+            ("date_end", ">=", today),
             ("state", "=", "active"),
         ]
-        aeat_certificate = self.search(domain)
-        if not aeat_certificate:
-            raise exceptions.UserError(self.env._("Error! There aren't certificates."))
+        domain_aeat = [("scope", "=", "aeat")]
+        domain_aeat = domain + domain_aeat
 
-        public_crt = aeat_certificate.certificate_id.pem_certificate
-        private_key_record = aeat_certificate.certificate_id.private_key_id
+        aeat_certificate = self.env["certificate.certificate"].search(domain_aeat)
+        if not aeat_certificate:
+            aeat_certificate = self.env["certificate.certificate"].search(domain)
+        if not aeat_certificate:
+            raise UserError(self.env._("Error! There aren't certificates."))
+
+        public_crt = aeat_certificate.pem_certificate
+        private_key_record = aeat_certificate.private_key_id
         if not private_key_record or not private_key_record.pem_key:
-            raise exceptions.UserError(self.env._("Private key is missing or invalid."))
+            raise UserError(self.env._("Private key is missing or invalid."))
 
         private_key = private_key_record.pem_key
 
